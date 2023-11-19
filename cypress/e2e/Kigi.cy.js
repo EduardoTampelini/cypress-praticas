dc347b9b711879b360dc0bbb28bcad68942f4dad
Eduardo Tampelini <ezafalon90@gmail.com>
2023-11-19 14:38:41 -0300

    up

--- cypress/e2e/Kigi.cy.js
+++ cypress/e2e/Kigi.cy.js
@@ -7,8 +7,8 @@ describe('teste kigi', function(){
     
      cy.visit('https://app.kigi.com.br/#!/login');
      cy.wait(4000)
-    cy.get('input[name="email"]').eq(1).clear().type('suporte13@grands.com.br')
-    cy.get('input[name="passw"]').type('123')
+    cy.get('input[name="email"]').eq(1).clear().type(Cypress.env('login'))
+    cy.get('input[name="passw"]').type(Cypress.env('senha'))
     cy.get('form.ng-dirty > mbg-login-send-button.ng-isolate-scope > .send-button > .text-container').click({force: true})
     cy.get('.organizations-container > :nth-child(1)').click({force: true})
     cy.get('.bottom > .ng-isolate-scope > .send-button > .text-container').click({force: true})
